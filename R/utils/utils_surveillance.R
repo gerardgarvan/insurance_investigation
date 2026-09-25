f29711ac4d0727bddbdb61cba64c320e789ae93f
gerardgarvan <73901673+gerardgarvan@users.noreply.github.com>
2026-09-25 09:44:57 -0400

    fix(159): error msg contains both 'Duplicate' and 'unique' for Phase 158 tests

--- R/utils/utils_surveillance.R
+++ R/utils/utils_surveillance.R
@@ -148,7 +148,7 @@ load_surveillance_codeset <- function(
 
   dup <- duplicated(cs[, c("modality", "code_norm", "match")])
   if (any(dup))
-    stop("Non-unique modality x code_norm: ",
+    stop("Duplicate (non-unique) modality x code_norm: ",
          paste(cs$modality[dup], cs$code_norm[dup], sep = "/", collapse = "; "))
 
   tibble::as_tibble(cs)
